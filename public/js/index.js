cb43df3ca6480418ec04c2e5bc68343d0b6e001d
michael <mf@fabrik19.de>
2020-06-09 12:58:06 +0200

    mixer name = mixer uid again

--- public/js/index.js
+++ public/js/index.js
@@ -86,8 +86,7 @@ function getDimensionsSelect(name, width, height) {
     var currentDimensions = width && height ? width + 'x' + height : null
     var dimensionsOptions = {}
     dimensionsOptions[currentDimensions] = prettyDimensions({width: width, height: height})
-    console.log({standardDimensions})
-    standardDimensions.filer(d => Array.isArray(d)).forEach(d => {
+    standardDimensions.filter(d => Array.isArray(d)).forEach(d => {
         dimensions = d[0] + 'x' + d[1]
         dimensionsOptions[dimensions] = prettyDimensions({width: d[0], height: d[1]})
     })
