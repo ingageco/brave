f073e28bfd218e9dfb12d0500d55ed78b782fc22
michael <mf@fabrik19.de>
2020-06-10 14:24:21 +0200

    fb login button

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -13,7 +13,7 @@ FBWrapper = {
 FBWrapper.init = () => {
     FB.init({appId: '674923560015547', version: 'v7.0', cookie: true, xfbml: true})
     FB.AppEvents.logPageView()
-    FB.getLoginStatus(response => FBWrapper.changeLogin(response))
+    // FB.getLoginStatus(response => FBWrapper.changeLogin(response))
     FB.Event.subscribe('auth.statusChange', response => FBWrapper.changeLogin(response));
 }
 
