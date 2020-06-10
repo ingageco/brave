31d5a2679eb2083c0be8d7bc1773855307544715
michael <mf@fabrik19.de>
2020-06-10 12:06:40 +0200

    fb login button

---
+++ brave/outputs/facebook.py
@@ -0,0 +1,4 @@
+# https://developers.facebook.com/docs/graph-api/reference/page/live_videos
+# use api to retrieve url(s) -> create output(s) (backup stream? checkbox for this!)
+# display an (optional) overlay for the comments
+# + display linked url to live video
