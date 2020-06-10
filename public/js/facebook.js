3ec5b1d6e964e4dc84927714824683b71e91063b
michael <mf@fabrik19.de>
2020-06-10 14:22:04 +0200

    fb login button

--- public/js/facebook.js
+++ public/js/facebook.js
@@ -1,4 +1,5 @@
 FBWrapper = {
+    lastStatus: 'unknown',
     token: {
         bearer: null,
         userID: null,
@@ -12,25 +13,22 @@ FBWrapper = {
 FBWrapper.init = () => {
     FB.init({appId: '674923560015547', version: 'v7.0', cookie: true, xfbml: true})
     FB.AppEvents.logPageView()
-    FB.getLoginStatus(response => FBWrapper.changeLogin(response, true))
+    FB.getLoginStatus(response => FBWrapper.changeLogin(response))
     FB.Event.subscribe('auth.statusChange', response => FBWrapper.changeLogin(response));
 }
 
-FBWrapper.changeLogin = (response, initial = false) => {
+FBWrapper.changeLogin = (response) => {
     console.log({response, initial})
-    if (response.status === 'connected') {
-        FBWrapper.token.bearer = response.authResponse.accessToken
-        FBWrapper.token.userID = response.authResponse.userID
-        // expiresIn are seconds, we want expiresAt in ms
-        FBWrapper.token.expiresAt = Date.now() + (response.authResponse.expiresIn * 1000)
-        FBWrapper.token.dateAccessExpiresAt = response.authResponse.data_access_expiration_time
-        outputsHandler.possibleOutputs.facebook = 'Facebook Connect + Reactions';
-
-    } else {
-        if (!initial) {
-            // just reload page, if we got a logout event
-            console.log('RELOAD')
-            // location.reload()
+    if (FBWrapper.lastStatus !== response.status) {
+        if (response.status === 'connected') {
+            FBWrapper.token.bearer = response.authResponse.accessToken
+            FBWrapper.token.userID = response.authResponse.userID
+            // expiresIn are seconds, we want expiresAt in ms
+            FBWrapper.token.expiresAt = Date.now() + (response.authResponse.expiresIn * 1000)
+            FBWrapper.token.dateAccessExpiresAt = response.authResponse.data_access_expiration_time
+            outputsHandler.possibleOutputs.facebook = 'Facebook Connect + Reactions';
+        } else if (!initial) {
+            location.reload() // reload after logout
         }
     }
 }
