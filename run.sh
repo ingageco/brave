e4d06c6daa3581ccaf394baf11e14204c570be79
Jay Hughes <jay@ingage.co>
2021-10-04 14:12:55 -0400

    updated

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 eval "$(/root/miniconda/bin/conda shell.bash hook)"
 conda activate brave
 cd /root/brave
-pipenv run ./brave.py -c /home/ottes/braveconfig.yaml
+pipenv run ./brave.py -c ./config/default.yaml
